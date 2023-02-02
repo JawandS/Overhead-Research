7ed61e036a1e3c2d99f4162d00a0992589643fb9
Jawand <jawandsingh@gmail.com>
2023-02-02 18:57:18 -0500

    rename

--- exp.sh
+++ exp.sh
@@ -55,6 +55,7 @@ for _ in {1..10}; do # number of iterations
 done
 python3 process.py "$1" $iterations $increment $threads $depth "$2" # run number, iterations, time, threads, depth, governor
 python3 visualizer.py "$1" # run number
+git pull
 git add .
 git commit -m "add and process overhead experiment $1"
 git push # add to git
--- visualizer.py
+++ visualizer.py
@@ -5,65 +5,6 @@ import pandas as pd
 import seaborn as sns
 
 
-def versionOne(run_num):
-    with open(f"Results/result_{run_num}.txt") as file:
-        #  get data
-        lines = [line.rstrip() for line in file]
-        # TIME = 20  # length of each job set
-        twoProbes = [elem for elem in eval(lines[8])]
-        tracing = [elem for elem in eval(lines[10])]
-        standard = [elem for elem in eval(lines[12])]
-        runNums = [i + 1 for i in range(len(twoProbes))]
-        data_preproc = pd.DataFrame(
-            {"Run": runNums, "Two Probes": [((twoProbes[i] / standard[i]) - 1) * 100 for i in range(len(standard))],
-             "Tracing": [((tracing[i] / standard[i]) - 1) * 100 for i in range(len(standard))],
-             "Standard": [((standard[i] / standard[i]) - 1) * 100 for i in range(len(standard))]})
-        # plot data
-        fig = sns.scatterplot(x='Run', y='value', hue='variable', data=pd.melt(data_preproc, ['Run']))
-        fig.set(xlabel='Iteration', ylabel='Change from Standard (%)')
-        fig.set_xticks([i for i in range(1, len(runNums) + 1)])
-        fig.legend(loc='center right')
-        fig.get_figure().savefig(f"Figures/figure_{run_num}.png")
-        # close the plot
-        plt.close()
-
-
-def versionTwo(run_num):
-    # add bar plots of relative jobs/events for powersave mode
-    with open(f"Results/result_{run_num}.txt") as file:
-        #  get data
-        lines = [line.rstrip() for line in file]
-        relJobs = eval(lines[2])
-        relEvents = eval(lines[4])
-        df = pd.DataFrame({
-            'Type': ["X", "A", "B", "C", "D", "E"],
-            'Relative # of Jobs': [elem for elem in relJobs],
-            'Relative # of Events': [elem for elem in relEvents]
-        })
-        fig, ax1 = plt.subplots(figsize=(10, 10))
-        tidy = df.melt(id_vars='Type').rename(columns=str.title)
-        sns.barplot(x='Type', y='Value', hue='Variable', data=tidy, ax=ax1)
-        sns.despine(fig)
-        fig.savefig(f"Figures/Powersave/figure_{run_num}.png")
-
-
-def versionThree(run_num):
-    # add bar plots of relative jobs/events for powersave mode
-    with open(f"Results/result_{run_num}.txt") as file:
-        #  get data
-        lines = [line.rstrip() for line in file]
-        relJobs = eval(lines[6])
-        relEvents = eval(lines[8])
-        df = pd.DataFrame({
-            'Type': ["X", "A", "B", "C", "D", "E"],
-            'Total Jobs': [elem for elem in relJobs],
-        })
-        fig, ax1 = plt.subplots(figsize=(10, 10))
-        sns.barplot(x='Type', y='Total Jobs', data=df, ax=ax1)
-        sns.despine(fig)
-        fig.savefig(f"Figures/Powersave/figure_abs_{run_num}.png")
-
-
 def linePlot(run_num):
     # line plot of the events compared to jobs and probes
     with open(f"Results/result_{run_num}.txt") as file:
@@ -82,23 +23,22 @@ def linePlot(run_num):
         fig, ax1 = plt.subplots(figsize=(10, 10))
         sns.regplot(x='Total Events', y='Total Jobs', data=df, ax=ax1)
         sns.despine(fig)
-        fig.savefig(f"Figures/events_to_jobs_{run_num}.png")
+        fig.savefig(f"Figures/{run_num}_events.png")
         # close the plot
         plt.close()
         # plot probes to jobs
         fig, ax1 = plt.subplots(figsize=(10, 10))
         sns.regplot(x='Probes', y='Total Jobs', data=df, ax=ax1)
         sns.despine(fig)
-        fig.savefig(f"Figures/probes_to_jobs_{run_num}.png")
+        fig.savefig(f"Figures/{run_num}_probes.png")
 
 
 if __name__ == "__main__":
     args = sys.argv
     if len(args) > 1:
         run_num = args[1]
-        versionThree(run_num)
+        linePlot(run_num)
     else:
-        # run_nums = ["2", "3", "4", "5", "C1", "C2"]
-        run_nums = ["home_1_ps"]
+        run_nums = []
         for run_num in run_nums:
             linePlot(run_num)
