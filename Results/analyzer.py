a30976f4168edbf6f969c1e165f688d0940d09e2
Jawand <jawandsingh@gmail.com>
2023-03-06 19:03:32 -0500

    update analyzer to work on VSCode

--- Results/analyzer.py
+++ Results/analyzer.py
@@ -5,16 +5,17 @@ import matplotlib.pyplot as plt
 
 
 # read in data from all experiments
-def visualize(serverType, governor="", experiment="csExp"):
+def visualize(serverType, governor="", experiment="probesExp"):
     allJobs = []
     allEvents = []
-    for file in os.listdir(experiment):
+    path = os.getcwd() + "\\Results\\" + experiment
+    for file in os.listdir(path):
         if serverType in file and governor in file:
-            lines = open(experiment + "/" + file, "r").readlines()
+            lines = open(path + "\\" + file, "r").readlines()
             allJobs.append(eval(lines[19]))
             if experiment == "probesExp":
                 allEvents.append(eval(lines[21]))
-            else: # context switch experiment
+            else:  # context switch experiment
                 allEvents.append([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] * 10)
 
     # create a scatter plot of the data
@@ -30,17 +31,15 @@ def visualize(serverType, governor="", experiment="csExp"):
     # fig.savefig(f"Figures/{idVal}_eventsToJobs.png")
 
 
-servers = [("aws", ""), ("cloudlab", ""), ("home", "per"), ("home", "ps"), ("school", "per"), ("school", "ps")]
+servers = [("aws", ""), ("cloudlab", ""), ("home", "per"),
+           ("home", "ps"), ("school", "per"), ("school", "ps")]
 serversNoGov = [("aws", ""), ("cloudlab", ""), ("home", ""), ("school", "")]
-allServers = [("aws", ""), ("cloudlabA", ""), ("cloudlabB", "per"), ("cloudlabB", "ps"), ("home", "per"), ("home", "ps"), ("school", "per"), ("school", "ps")]
-for server, governor in servers:
-    # visualize(server, governor, "csExp")
-    # visualize(server, governor, "probesExp")
-    visualize(server, governor, "csExp")
-
-# serverTypes = [("aws", "per", "home", "school", "cloudlab", "cloudlabA", "cloudlabC"]
-# print([f"{idx}: {serverType}" for idx, serverType in enumerate(serverTypes)])
-# idVal = int(input("Server number: "))
-# governor = ""
-# if serverTypes[idVal] in ["home", "school", "cloudlabB"]:
-#     governor = input("Governor Types (per/ps): ")
+allServers = [("aws", ""), ("cloudlabA", ""), ("cloudlabB", "per"), ("cloudlabB",
+                                                                     "ps"), ("home", "per"), ("home", "ps"), ("school", "per"), ("school", "ps")]
+experimentType = ["probesExp", "csExp"][int(input("probesExp (0) or csExp(1): "))]
+if experimentType == "csExp":
+    for server, governor in servers:
+        visualize(server, governor, experimentType)
+else:
+    for server, governor in allServers:
+        visualize(server, governor, experimentType)
