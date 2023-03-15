74ce3da75844a0067d08a16b3c14a4b99d27ee34
Jawand <jawandsingh@gmail.com>
2023-03-15 08:54:57 -0400

    adjust ranges

--- csCoresExp.sh
+++ csCoresExp.sh
@@ -1,5 +1,5 @@
 # cores = range i.e. 0-1
-$cores=0,1
+$cores=0-1
 sudo taskset -c $cores ./csExp.sh 'school"$cores"_1_per' performance
 sleep(3)
 sudo taskset -c $cores ./csExp.sh 'school"$cores"_2_ps' powersave
@@ -8,7 +8,7 @@ sudo taskset -c $cores ./csExp.sh 'school"$cores"_3_per' performance
 sleep(3)
 sudo taskset -c $cores ./csExp.sh 'school"$cores"_4_ps' powersave
 sleep(3)
-$cores=0,1,2,3
+$cores=0-3
 sudo taskset -c $cores ./csExp.sh 'school"$cores"_1_per' performance
 sleep(3)
 sudo taskset -c $cores ./csExp.sh 'school"$cores"_2_ps' powersave
@@ -17,7 +17,7 @@ sudo taskset -c $cores ./csExp.sh 'school"$cores"_3_per' performance
 sleep(3)
 sudo taskset -c $cores ./csExp.sh 'school"$cores"_4_ps' powersave
 sleep(3)
-$cores=0,1,2,3,4,5,6,7
+$cores=0-7
 sudo taskset -c $cores ./csExp.sh 'school"$cores"_1_per' performance
 sleep(3)
 sudo taskset -c $cores ./csExp.sh 'school"$cores"_2_ps' powersave
